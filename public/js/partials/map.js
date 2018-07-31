7bf5756691d3c0e0fcb7c11f0b3d0a7d9e9cd2ff
josue rojas <josuerojas.rojas@gmail.com>
2018-07-31 19:49:17 -0400

    basic design for sidebar and map in mobile

---
+++ public/js/partials/map.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken = 'pk.eyJ1Ijoid2l0aGNoZWVzZXBscyIsImEiOiJjams1cTkybmcwamo5M3FwMm5jMjdzdHl3In0.yI9WNY7aCdeQndyxbU9Amg';
+var map = new mapboxgl.Map({
+container: 'map',
+style: 'mapbox://styles/mapbox/streets-v10',
+hash: true
+});
---
+++ public/js/partials/sidebar.js
@@ -0,0 +1,29 @@
+let isBottom = false;
+// just to make sure it is at top...
+$('html, body').animate({scrollTop: 0}, 'slow');
+
+$('.sidebar .menu-button').click((e)=>{
+  let $sidebar = $(e.target).closest('.sidebar');
+  $sidebar.toggleClass('active');
+  if(isBottom){
+    $('html, body').animate({scrollTop: 0}, 'slow');
+    isBottom = false;
+  }
+  else{
+    $('html, body').animate({scrollTop: $(document).height()}, 'slow');
+    isBottom = true;
+  }
+});
+
+
+
+const $window = $(window);
+
+$window.scroll(function() {
+   if($window.scrollTop() + $window.height() > $(document).height()-1) {
+     $('.sidebar').addClass('active');
+   }
+   else{
+     $('.sidebar').removeClass('active');
+   }
+});

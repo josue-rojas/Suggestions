a0bd2dd196cc0533749bb4770e043001b5168c71
josue rojas <josuerojas.rojas@gmail.com>
2018-08-06 00:20:01 -0400

    simple post design

--- public/js/partials/sidebar.js
+++ public/js/partials/sidebar.js
@@ -1,31 +1,15 @@
-let isBottom = false;
-// just to make sure it is at top...
-$('html, body').animate({scrollTop: 0}, 'slow');
-
-$('.sidebar .menu-button').click((e)=>{
-  let $sidebar = $(e.target).closest('.sidebar');
-  $sidebar.toggleClass('active');
-  if(isBottom){
-    $('html, body').animate({scrollTop: 0}, 'slow');
-    isBottom = false;
-  }
-  else{
-    $('html, body').animate({scrollTop: $(document).height()}, 'slow');
-    isBottom = true;
-  }
-});
-
-
-
 const $window = $(window);
 
-$window.scroll(function() {
-   if($window.scrollTop() + $window.height() > $(document).height()-1) {
-     $('.sidebar').addClass('active');
-   }
-   else{
-     $('.sidebar').removeClass('active');
-   }
+// TODO: make transition smoother
+$window.scroll((e)=>{
+  const $sidebar = $('.sidebar');
+ // $sidebar.outerHeight()+($window.outerHeight()*.7)- $window.outerHeight());
+  if($(document).height() == $(window).scrollTop() + $(window).height()){
+    $sidebar.removeClass('noscroll')
+  }
+  else if(!$sidebar.hasClass('noscroll')){
+    $sidebar.addClass('noscroll');
+  }
 });
 
 // -------------------------------
@@ -51,4 +35,5 @@ $window.resize(()=>{
 // document ready stuff
 $(document).ready(()=>{
   addGeocoder($window.width());
+
 })
